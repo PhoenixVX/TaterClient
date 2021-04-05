579b2796fe37c81ff6a735c232e40fd3cd381d1b
PheonixVX <TheGrimReaper.VX@gmail.com>
2021-04-05 12:09:16 -0400

    Base

---
+++ src/main/java/io/github/pheonixvx/taterclient/taterclient/TaterClient.java
@@ -0,0 +1,10 @@
+package io.github.pheonixvx.taterclient.taterclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TaterClient implements ModInitializer {
+	@Override
+	public void onInitialize () {
+
+	}
+}
---
+++ src/main/java/io/github/pheonixvx/taterclient/taterclient/client/TaterClientClient.java
@@ -0,0 +1,13 @@
+package io.github.pheonixvx.taterclient.taterclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TaterClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient () {
+
+	}
+}
